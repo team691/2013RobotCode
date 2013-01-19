2987034921c101ec3930c62713c527ccaad4f194
Dare1024 <dring@g33kworld.net>
2013-01-19 14:34:39 -0800

    Added Uptake.java for uptake support. Changed shooter controls (buttons 4 & 5 are now both full speed). Added sidecar numbers for shooter victor and tilt victor in constructors and Values.java.

--- Team691DiscShooter/src/org/usfirst/frc691/discshooter/Robot.java
+++ Team691DiscShooter/src/org/usfirst/frc691/discshooter/Robot.java
@@ -6,21 +6,31 @@ import edu.wpi.first.wpilibj.SimpleRobot;
 
 public class Robot extends SimpleRobot {
 
+    //Driver station I/O
     Joystick joy = new Joystick(Values.SHOOTER_JOYSTICK);
     DriverStationLCD dslcd = DriverStationLCD.getInstance();
+    
+    //Shooter, Uptake, & Intake
     Shooter shooter;
+    Uptake uptake;
 
     public void robotInit() {
         shooter = new Shooter(
                 Values.SHOOTER_VICTOR,
+                Values.SHOOTER_VICTOR_SIDECAR,
                 Values.SHOOTER_ENCODER,
-                Values.SHOOTER_TILT_VICTOR);
+                Values.SHOOTER_TILT_VICTOR, 
+                Values.SHOOTER_TILT_VICTOR_SIDECAR);
         shooter.stop();
+        
+        uptake = new Uptake(Values.UPTAKE_VICTOR_SIDECAR, Values.UPTAKE_VICTOR);
+        uptake.stop();
     }
 
     public void operatorControl() {
         while (isEnabled() && isOperatorControl()) {
             shooter.shoot(joy);
+            uptake.feed(joy);
             
             dslcd.println(DriverStationLCD.Line.kMain6, 1, Double.toString(shooter.getSpeed()));
             dslcd.updateLCD();
--- Team691DiscShooter/src/org/usfirst/frc691/discshooter/Values.java
+++ Team691DiscShooter/src/org/usfirst/frc691/discshooter/Values.java
@@ -5,6 +5,7 @@ public class Values {
     public static final int SHOOTER_JOYSTICK        = 2;
     
     public static final int SHOOTER_VICTOR          = 0;
+    public static final int SHOOTER_VICTOR_SIDECAR  = 0;
     
     public static final int SHOOTER_ENCODER_A       = 0;
     public static final int SHOOTER_ENCODER_B       = 0;
@@ -12,5 +13,9 @@ public class Values {
     public static final int SHOOTER_ENCODER_COUNT   = 0;
     public static final int[] SHOOTER_ENCODER       = {SHOOTER_ENCODER_SIDECAR, SHOOTER_ENCODER_A, SHOOTER_ENCODER_B, SHOOTER_ENCODER_COUNT};
     
-    public static final int SHOOTER_TILT_VICTOR     = 0;
+    public static final int SHOOTER_TILT_VICTOR         = 0;
+    public static final int SHOOTER_TILT_VICTOR_SIDECAR = 0;
+    
+    public static final int UPTAKE_VICTOR           = 0;
+    public static final int UPTAKE_VICTOR_SIDECAR   = 0;
 }
