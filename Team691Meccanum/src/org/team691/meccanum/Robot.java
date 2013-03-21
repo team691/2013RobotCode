655cb2e21fbd605f4f4ff2eb9a24c3726ade1982
Dare1024 <dring@g33kworld.net>
2013-03-21 11:03:43 -0700

    Added code to Robot.java to drive forward for 2 seconds at the end of autonomous. Changed name of joystick in Robot.java to from joy to driveJoy for better portability. Removed unnecessary comment from PIDVelocityMotor.java.

--- Team691Meccanum/src/org/team691/meccanum/Robot.java
+++ Team691Meccanum/src/org/team691/meccanum/Robot.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.SimpleRobot;
 public class Robot extends SimpleRobot {
 	
     //Joysticks
-    Joystick joy = new Joystick(Values.DRIVE_JOYSTICK);
+    Joystick driveJoy = new Joystick(Values.DRIVE_JOYSTICK);
     double forward = 0.0;
     double right = 0.0;
     double clockwise = 0.0;
@@ -15,6 +15,9 @@ public class Robot extends SimpleRobot {
 
     //Meccanum drive
     Meccanum drive;
+    
+    //Autonomous
+    long autoStart = 0;
 
     public void robotInit() {
         //drive = new Meccanum(Values.DRIVE_VICTOR_SIDECARS, Values.DRIVE_VICTORS); //No encoders
@@ -30,23 +33,34 @@ public class Robot extends SimpleRobot {
         drive.stop();
         scalar =  (Values.FR_DRIVE_PID_SCALAR + Values.FL_DRIVE_PID_SCALAR + Values.BR_DRIVE_PID_SCALAR + Values.BL_DRIVE_PID_SCALAR) / 4;
     }
+    
+    public void autonomous() {
+        autoStart = System.currentTimeMillis();
+        while(isEnabled() && isAutonomous()) {            
+            if(((System.currentTimeMillis() - autoStart) > 12000) && ((System.currentTimeMillis() - autoStart) < 14000)) {
+                drive.update(-0.25, 0.0, 0.0);  //Drive Forward
+            } else {
+                drive.update(0.0, 0.0, 0.0);    //Stop
+            }
+        }
+    }
 	
     public void operatorControl() {
         while(isEnabled() && isOperatorControl()) {
-            if(Math.abs(joy.getRawAxis(2)) < 0.2) {
+            if(Math.abs(driveJoy.getRawAxis(2)) < 0.2) {
                 forward = 0.0;
             } else {
-                forward = joy.getRawAxis(2) * scalar;
+                forward = driveJoy.getRawAxis(2) * scalar;
             }
-            if(Math.abs(joy.getRawAxis(1)) < 0.2) {
+            if(Math.abs(driveJoy.getRawAxis(1)) < 0.2) {
                 right = 0.0;
             } else {
-                right = joy.getRawAxis(1) * scalar;
+                right = driveJoy.getRawAxis(1) * scalar;
             }
-            if(Math.abs(joy.getRawAxis(3)) < 0.2) {
+            if(Math.abs(driveJoy.getRawAxis(3)) < 0.2) {
                 clockwise = 0.0;
             } else {    // if(Math.abs(joy.getRawAxis(3)) < 0.75) {
-                clockwise = joy.getRawAxis(3) * 0.75 * scalar;
+                clockwise = driveJoy.getRawAxis(3) * 0.75 * scalar;
             }/* else {
                 clockwise = joy.getRawAxis(3);
             }*/
