900df7c2ad9fc6dd0f47ca56d21be48937b39010
Dare1024 <dring@g33kworld.net>
2013-04-13 11:34:25 -0700

    Removed drive PID from Robot.java.

--- Team691Meccanum/src/org/team691/meccanum/Robot.java
+++ Team691Meccanum/src/org/team691/meccanum/Robot.java
@@ -14,14 +14,14 @@ public class Robot extends SimpleRobot {
     double forward = 0.0;
     double right = 0.0;
     double clockwise = 0.0;
-    double scalar = 0.0;
+    //double scalar = 0.0;
     
     //Autonomous
     long autoStart = 0;
 
     public void robotInit() {
-        //drive = new Meccanum(Values.DRIVE_VICTOR_SIDECARS, Values.DRIVE_VICTORS); //No encoders
-        drive = new Meccanum(
+        drive = new Meccanum(Values.DRIVE_VICTOR_SIDECARS, Values.DRIVE_VICTORS); //No encoders
+        /*drive = new Meccanum(
                         Values.DRIVE_VICTOR_SIDECARS,
                         Values.DRIVE_VICTORS,
                         Values.DRIVE_ENCODER_SIDECARS,
@@ -29,9 +29,9 @@ public class Robot extends SimpleRobot {
                         Values.DRIVE_ENCODER_DISTANCES_PER_PULSE,
                         Values.DRIVE_ENCODER_REVERSES,
                         Values.DRIVE_PID
-        );  //With encoders
+        );*/  //With encoders
         drive.stop();
-        scalar = (Values.FR_DRIVE_PID_SCALAR + Values.FL_DRIVE_PID_SCALAR + Values.BR_DRIVE_PID_SCALAR + Values.BL_DRIVE_PID_SCALAR) / 4;
+        //scalar = (Values.FR_DRIVE_PID_SCALAR + Values.FL_DRIVE_PID_SCALAR + Values.BR_DRIVE_PID_SCALAR + Values.BL_DRIVE_PID_SCALAR) / 4;
     }
     
     public void autonomous() {
@@ -52,14 +52,14 @@ public class Robot extends SimpleRobot {
             } else {
                 forward = driveJoy.getRawAxis(2);
                 forward *= Math.abs(forward);
-                forward *= scalar;
+                //forward *= scalar;
             }
             if(Math.abs(driveJoy.getRawAxis(1)) < 0.2) {
                 right = 0.0;
             } else {
                 right = driveJoy.getRawAxis(1);
                 right *= Math.abs(right);
-                right *= scalar;
+                //right *= scalar;
             }
             if(Math.abs(driveJoy.getRawAxis(3)) < 0.2) {
                 clockwise = 0.0;
@@ -70,7 +70,7 @@ public class Robot extends SimpleRobot {
                 } else {
                     clockwise *= Math.abs(clockwise);
                 }
-                clockwise *= scalar;
+                //clockwise *= scalar;
             }
             drive.update(forward, right, -clockwise);
                        //Forward  Right  Clockwise
